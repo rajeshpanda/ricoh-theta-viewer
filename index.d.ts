48cbab42ccc8672effc109cfd2379227eb4ccb46
Rajesh Panda <therajeshpanda@gmail.com>
2017-07-24 19:40:34 +0530

    Initial draft

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+export default class ThView {
+    constructor(args);
+
+    toggleRotation();
+
+    rotateCamera(x, y);
+
+    setCameraDir(alpha, beta, gamma);
+
+    zoomCamera(val);
+
+    show();
+
+}
